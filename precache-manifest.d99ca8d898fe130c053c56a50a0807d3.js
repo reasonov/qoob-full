b20c6e6d92467c9644a4547bb631e79a9cc53695
reasonov1k <iluayout@gmail.com>
2022-03-29 19:25:43 +1000

    first commit

---
+++ precache-manifest.d99ca8d898fe130c053c56a50a0807d3.js
@@ -0,0 +1,610 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37fc888dbf6d274cf4a6",
+    "url": "/qoob-full/css/app.767b41fb.css"
+  },
+  {
+    "revision": "3ee36787a3be214477c0",
+    "url": "/qoob-full/css/chunk-vendors.de58d00f.css"
+  },
+  {
+    "revision": "0e0d4042480441b96b345cca7025e8b6",
+    "url": "/qoob-full/fonts/GraphikLCG-Black.0e0d4042.eot"
+  },
+  {
+    "revision": "518fff7cfe1596a283296b892ae8c18f",
+    "url": "/qoob-full/fonts/GraphikLCG-Black.518fff7c.woff2"
+  },
+  {
+    "revision": "ba664de4aa0993067ecdbdc52fffb5d2",
+    "url": "/qoob-full/fonts/GraphikLCG-Black.ba664de4.ttf"
+  },
+  {
+    "revision": "efe98be2dec476ecd60c8a1ebd960d8c",
+    "url": "/qoob-full/fonts/GraphikLCG-Black.efe98be2.woff"
+  },
+  {
+    "revision": "0c5a39db3353cab2f504fc85a04c646f",
+    "url": "/qoob-full/fonts/GraphikLCG-BlackItalic.0c5a39db.eot"
+  },
+  {
+    "revision": "99e957b22b9dc6570a445f8f582b493b",
+    "url": "/qoob-full/fonts/GraphikLCG-BlackItalic.99e957b2.ttf"
+  },
+  {
+    "revision": "a0e712c0ca4774ace98d44ce567ee141",
+    "url": "/qoob-full/fonts/GraphikLCG-BlackItalic.a0e712c0.woff2"
+  },
+  {
+    "revision": "cf0f27e483bc9ba8aa5a8799198240ee",
+    "url": "/qoob-full/fonts/GraphikLCG-BlackItalic.cf0f27e4.woff"
+  },
+  {
+    "revision": "0a863dc677dda8555d473fda348abb84",
+    "url": "/qoob-full/fonts/GraphikLCG-Bold.0a863dc6.woff2"
+  },
+  {
+    "revision": "2239e7c3d2c3cfc89772be87c4626163",
+    "url": "/qoob-full/fonts/GraphikLCG-Bold.2239e7c3.eot"
+  },
+  {
+    "revision": "b47218579b9c54f96439558471be8346",
+    "url": "/qoob-full/fonts/GraphikLCG-Bold.b4721857.ttf"
+  },
+  {
+    "revision": "ccf9f0ec441f4f4dbe130bba2b2a3ba3",
+    "url": "/qoob-full/fonts/GraphikLCG-Bold.ccf9f0ec.woff"
+  },
+  {
+    "revision": "20337ebd72de50348b23785a121bb6f1",
+    "url": "/qoob-full/fonts/GraphikLCG-BoldItalic.20337ebd.eot"
+  },
+  {
+    "revision": "51a4afa632ccd8d7831f6481183ffff6",
+    "url": "/qoob-full/fonts/GraphikLCG-BoldItalic.51a4afa6.woff"
+  },
+  {
+    "revision": "a2bb67267fe03612c458e9a393ac3040",
+    "url": "/qoob-full/fonts/GraphikLCG-BoldItalic.a2bb6726.woff2"
+  },
+  {
+    "revision": "cac603fddbacaa0c4e4e4490d03a33f8",
+    "url": "/qoob-full/fonts/GraphikLCG-BoldItalic.cac603fd.ttf"
+  },
+  {
+    "revision": "454a269e8f5d68dd8c315e6bdc91bd49",
+    "url": "/qoob-full/fonts/GraphikLCG-Extralight.454a269e.woff2"
+  },
+  {
+    "revision": "9b4d4436c3e9ee9117a15f2ebbe9934a",
+    "url": "/qoob-full/fonts/GraphikLCG-Extralight.9b4d4436.woff"
+  },
+  {
+    "revision": "bf63856263799620fa33c334b987d19e",
+    "url": "/qoob-full/fonts/GraphikLCG-Extralight.bf638562.eot"
+  },
+  {
+    "revision": "c1af7132dc967acba175aae517a71f9a",
+    "url": "/qoob-full/fonts/GraphikLCG-Extralight.c1af7132.ttf"
+  },
+  {
+    "revision": "0e3496ae2d16fc89d9c45a2b77208021",
+    "url": "/qoob-full/fonts/GraphikLCG-ExtralightItalic.0e3496ae.ttf"
+  },
+  {
+    "revision": "764e5bc540e33f23d29717128c1a2b9a",
+    "url": "/qoob-full/fonts/GraphikLCG-ExtralightItalic.764e5bc5.eot"
+  },
+  {
+    "revision": "776f6040e97df89a755624fd039f9656",
+    "url": "/qoob-full/fonts/GraphikLCG-ExtralightItalic.776f6040.woff2"
+  },
+  {
+    "revision": "cda5990a17193e2b2579170ecf509feb",
+    "url": "/qoob-full/fonts/GraphikLCG-ExtralightItalic.cda5990a.woff"
+  },
+  {
+    "revision": "3f7f7c7b4ef0f665a2a42c1ec090940c",
+    "url": "/qoob-full/fonts/GraphikLCG-Light.3f7f7c7b.eot"
+  },
+  {
+    "revision": "91bde36b5ff31fa96a1ddecd38c2ed32",
+    "url": "/qoob-full/fonts/GraphikLCG-Light.91bde36b.woff2"
+  },
+  {
+    "revision": "afc50e8ac92a30d9ad36302be372ffbf",
+    "url": "/qoob-full/fonts/GraphikLCG-Light.afc50e8a.woff"
+  },
+  {
+    "revision": "d73bdf48de6599c388b8341e1489bcba",
+    "url": "/qoob-full/fonts/GraphikLCG-Light.d73bdf48.ttf"
+  },
+  {
+    "revision": "1583180f8d833e65a7801e7d72159bd4",
+    "url": "/qoob-full/fonts/GraphikLCG-LightItalic.1583180f.woff2"
+  },
+  {
+    "revision": "83b309579695f8df69230d4b31ab101f",
+    "url": "/qoob-full/fonts/GraphikLCG-LightItalic.83b30957.woff"
+  },
+  {
+    "revision": "8517e01b8215bf236787f4d6c453cdae",
+    "url": "/qoob-full/fonts/GraphikLCG-LightItalic.8517e01b.eot"
+  },
+  {
+    "revision": "dddbc19372d5c460407cabde135559a0",
+    "url": "/qoob-full/fonts/GraphikLCG-LightItalic.dddbc193.ttf"
+  },
+  {
+    "revision": "0d776a835fb10e6a6ce46f4ac1a0f6a4",
+    "url": "/qoob-full/fonts/GraphikLCG-Medium.0d776a83.woff"
+  },
+  {
+    "revision": "10b7bd9b829afb254fb3c2e56229ef8c",
+    "url": "/qoob-full/fonts/GraphikLCG-Medium.10b7bd9b.woff2"
+  },
+  {
+    "revision": "5d0a360b42e56a8faa2e80546ca728db",
+    "url": "/qoob-full/fonts/GraphikLCG-Medium.5d0a360b.ttf"
+  },
+  {
+    "revision": "f4fe0ff7dc54f4204fe1000cabf15c01",
+    "url": "/qoob-full/fonts/GraphikLCG-Medium.f4fe0ff7.eot"
+  },
+  {
+    "revision": "4f566b4b3b3eaaff49a6d792ee67a3f0",
+    "url": "/qoob-full/fonts/GraphikLCG-MediumItalic.4f566b4b.woff2"
+  },
+  {
+    "revision": "66155d617a2215ddd61406b9ee805ab0",
+    "url": "/qoob-full/fonts/GraphikLCG-MediumItalic.66155d61.eot"
+  },
+  {
+    "revision": "d736acb1fe1729df3762acfff5e168c3",
+    "url": "/qoob-full/fonts/GraphikLCG-MediumItalic.d736acb1.woff"
+  },
+  {
+    "revision": "e2d238430001f995eb44b0e5b398fa6f",
+    "url": "/qoob-full/fonts/GraphikLCG-MediumItalic.e2d23843.ttf"
+  },
+  {
+    "revision": "0f8c265818cb0301de89969bd9baa1ff",
+    "url": "/qoob-full/fonts/GraphikLCG-Regular.0f8c2658.eot"
+  },
+  {
+    "revision": "2f338b5ebd9e450c9e4cfcccb69100eb",
+    "url": "/qoob-full/fonts/GraphikLCG-Regular.2f338b5e.woff2"
+  },
+  {
+    "revision": "469bebb636812293efd25f9c3b051c16",
+    "url": "/qoob-full/fonts/GraphikLCG-Regular.469bebb6.ttf"
+  },
+  {
+    "revision": "b1ae0f61d24fe8581622919a15d8939b",
+    "url": "/qoob-full/fonts/GraphikLCG-Regular.b1ae0f61.woff"
+  },
+  {
+    "revision": "130794596a3b6ee958aca03c5c7434f6",
+    "url": "/qoob-full/fonts/GraphikLCG-RegularItalic.13079459.eot"
+  },
+  {
+    "revision": "5d583e71b2101b37de47db102781e069",
+    "url": "/qoob-full/fonts/GraphikLCG-RegularItalic.5d583e71.woff"
+  },
+  {
+    "revision": "a5ac6edd04a6f971fc445ff10070a6fc",
+    "url": "/qoob-full/fonts/GraphikLCG-RegularItalic.a5ac6edd.woff2"
+  },
+  {
+    "revision": "c94f6a585173623412c0b46f1cd83cd5",
+    "url": "/qoob-full/fonts/GraphikLCG-RegularItalic.c94f6a58.ttf"
+  },
+  {
+    "revision": "04581b0eb2bd3dba76bf8241f15433a4",
+    "url": "/qoob-full/fonts/GraphikLCG-Semibold.04581b0e.woff2"
+  },
+  {
+    "revision": "339c2e00787966774270be03e2e948d7",
+    "url": "/qoob-full/fonts/GraphikLCG-Semibold.339c2e00.woff"
+  },
+  {
+    "revision": "8f6e43d8111c11010e8c364c1961272e",
+    "url": "/qoob-full/fonts/GraphikLCG-Semibold.8f6e43d8.eot"
+  },
+  {
+    "revision": "f87eef8d94a73dc24de3f0a66d8d71c3",
+    "url": "/qoob-full/fonts/GraphikLCG-Semibold.f87eef8d.ttf"
+  },
+  {
+    "revision": "03f9b3f3a5e9cb2310ca177764eb7588",
+    "url": "/qoob-full/fonts/GraphikLCG-SemiboldItalic.03f9b3f3.woff2"
+  },
+  {
+    "revision": "2bb4fb82f1ce4aab8abbb088b35e7c99",
+    "url": "/qoob-full/fonts/GraphikLCG-SemiboldItalic.2bb4fb82.ttf"
+  },
+  {
+    "revision": "2d6f36c59c5295ea98c296eff82df97c",
+    "url": "/qoob-full/fonts/GraphikLCG-SemiboldItalic.2d6f36c5.eot"
+  },
+  {
+    "revision": "e66fdd037978981ea7a7fd00913a35a7",
+    "url": "/qoob-full/fonts/GraphikLCG-SemiboldItalic.e66fdd03.woff"
+  },
+  {
+    "revision": "813fa9537f00923615a35b9053f1d286",
+    "url": "/qoob-full/fonts/GraphikLCG-Super.813fa953.ttf"
+  },
+  {
+    "revision": "c5306116c2d8934b409bbbe76b731330",
+    "url": "/qoob-full/fonts/GraphikLCG-Super.c5306116.woff2"
+  },
+  {
+    "revision": "cf860eaafd34ab1171a56c5296991ab6",
+    "url": "/qoob-full/fonts/GraphikLCG-Super.cf860eaa.woff"
+  },
+  {
+    "revision": "e47a05cfdac52449db82eeeb816fa743",
+    "url": "/qoob-full/fonts/GraphikLCG-Super.e47a05cf.eot"
+  },
+  {
+    "revision": "02c470a78bc1be15a61363e591c700a9",
+    "url": "/qoob-full/fonts/GraphikLCG-SuperItalic.02c470a7.ttf"
+  },
+  {
+    "revision": "4d0a55d32d796705a215320818612628",
+    "url": "/qoob-full/fonts/GraphikLCG-SuperItalic.4d0a55d3.woff2"
+  },
+  {
+    "revision": "e1ca019386863e5c4c21c62d36cdab80",
+    "url": "/qoob-full/fonts/GraphikLCG-SuperItalic.e1ca0193.woff"
+  },
+  {
+    "revision": "f7896001e7e27446c477dad241bacb68",
+    "url": "/qoob-full/fonts/GraphikLCG-SuperItalic.f7896001.eot"
+  },
+  {
+    "revision": "132b198a697e989570055e6e8274a692",
+    "url": "/qoob-full/fonts/GraphikLCG-Thin.132b198a.eot"
+  },
+  {
+    "revision": "806b68313f9d78fcf601a2d1d32addc4",
+    "url": "/qoob-full/fonts/GraphikLCG-Thin.806b6831.ttf"
+  },
+  {
+    "revision": "d21200d9e7db03cc53f754b59a9f0aca",
+    "url": "/qoob-full/fonts/GraphikLCG-Thin.d21200d9.woff"
+  },
+  {
+    "revision": "feaba8224d02bccb32728e664f990dcf",
+    "url": "/qoob-full/fonts/GraphikLCG-Thin.feaba822.woff2"
+  },
+  {
+    "revision": "60e201c572e6bed0f6d0476e965cdcf9",
+    "url": "/qoob-full/fonts/GraphikLCG-ThinItalic.60e201c5.ttf"
+  },
+  {
+    "revision": "728584245d046a238b488676ba20374e",
+    "url": "/qoob-full/fonts/GraphikLCG-ThinItalic.72858424.woff2"
+  },
+  {
+    "revision": "81fefd79cfb83ba22f86518ea18b3903",
+    "url": "/qoob-full/fonts/GraphikLCG-ThinItalic.81fefd79.eot"
+  },
+  {
+    "revision": "85df45cb59004c82a6b6e11f60c5b161",
+    "url": "/qoob-full/fonts/GraphikLCG-ThinItalic.85df45cb.woff"
+  },
+  {
+    "revision": "3bb24c0037f38bf0de73b3c7bb114313",
+    "url": "/qoob-full/img/IconsetSmiles.3bb24c00.png"
+  },
+  {
+    "revision": "93f289139b43587ab55b89c77efe0351",
+    "url": "/qoob-full/img/IconsetW.93f28913.png"
+  },
+  {
+    "revision": "7b1ef418b993a93c48bd566a5d32a69f",
+    "url": "/qoob-full/img/IconsetW_1x.7b1ef418.png"
+  },
+  {
+    "revision": "cff29171b1bed4ad3357cc7f6908b770",
+    "url": "/qoob-full/img/QR.cff29171.jpg"
+  },
+  {
+    "revision": "e9da9b92abd32539399909ada1de86b7",
+    "url": "/qoob-full/img/add-picture.e9da9b92.svg"
+  },
+  {
+    "revision": "f86d1c6775c467b0d1e8c427eaea5ba3",
+    "url": "/qoob-full/img/add-square-list.f86d1c67.svg"
+  },
+  {
+    "revision": "7a48ecc72a2b895243bf0bfa896bcf61",
+    "url": "/qoob-full/img/add-square.7a48ecc7.svg"
+  },
+  {
+    "revision": "569784955958fb8501045171ed57b858",
+    "url": "/qoob-full/img/arrow-downpavel.56978495.svg"
+  },
+  {
+    "revision": "c69e35e95e8544ede97c325a77dabd9d",
+    "url": "/qoob-full/img/arrow-left-black.c69e35e9.svg"
+  },
+  {
+    "revision": "bec3546c32d7d7e194ec122018c919df",
+    "url": "/qoob-full/img/arrow-left-grey.bec3546c.svg"
+  },
+  {
+    "revision": "a5d990bc623599f8880fda2e4d90faa1",
+    "url": "/qoob-full/img/arrow-left-white.a5d990bc.svg"
+  },
+  {
+    "revision": "45f3c1b19254674e9824ae86c0aa98ed",
+    "url": "/qoob-full/img/arrow-right.45f3c1b1.svg"
+  },
+  {
+    "revision": "23e16a0daddf0b7c5a9112e05342aae6",
+    "url": "/qoob-full/img/arrow-select.23e16a0d.svg"
+  },
+  {
+    "revision": "bc6f3e7436aaace835ce87e10fb515a9",
+    "url": "/qoob-full/img/category.bc6f3e74.svg"
+  },
+  {
+    "revision": "c65f32df051b753ebd5adb833a9957e5",
+    "url": "/qoob-full/img/check.c65f32df.svg"
+  },
+  {
+    "revision": "f0d5d90b109a27d01c1ac041b1c13164",
+    "url": "/qoob-full/img/checkBox.f0d5d90b.svg"
+  },
+  {
+    "revision": "ca1d16ae0b9cf74cc8e5257d5ae51080",
+    "url": "/qoob-full/img/create-type-list.ca1d16ae.svg"
+  },
+  {
+    "revision": "862a1f268b70d4773ea077685bf818f0",
+    "url": "/qoob-full/img/create-type.862a1f26.svg"
+  },
+  {
+    "revision": "2415685939cdbb555a5c03af5316c686",
+    "url": "/qoob-full/img/dollar-square.24156859.svg"
+  },
+  {
+    "revision": "d39f67de78297bd625293a5adc9cc352",
+    "url": "/qoob-full/img/drop-down-sidebar.d39f67de.svg"
+  },
+  {
+    "revision": "13a8b90073220cc366ee91542267c009",
+    "url": "/qoob-full/img/drop-down.13a8b900.svg"
+  },
+  {
+    "revision": "cce267d042a31fa24dc933603de5a735",
+    "url": "/qoob-full/img/emoji_spritesheet_0.cce267d0.png"
+  },
+  {
+    "revision": "6cbaf42abb5720ecf531a62ab303743a",
+    "url": "/qoob-full/img/emoji_spritesheet_1.6cbaf42a.png"
+  },
+  {
+    "revision": "06aca5a634d281b50cecff0f69feacfc",
+    "url": "/qoob-full/img/emoji_spritesheet_2.06aca5a6.png"
+  },
+  {
+    "revision": "d76a9fcc24b7b68961edd44bfcf21973",
+    "url": "/qoob-full/img/emoji_spritesheet_3.d76a9fcc.png"
+  },
+  {
+    "revision": "9759ad2bdcde9327c495fc0f9be2dd05",
+    "url": "/qoob-full/img/emoji_spritesheet_4.9759ad2b.png"
+  },
+  {
+    "revision": "a0f6a9746ec67f13eaa67ea6dd969aed",
+    "url": "/qoob-full/img/frame873.a0f6a974.svg"
+  },
+  {
+    "revision": "9dc91c3f6babaf91ba5e2b747d45dee6",
+    "url": "/qoob-full/img/frame874.9dc91c3f.svg"
+  },
+  {
+    "revision": "fea62b93b786f60370bab7186384ba8d",
+    "url": "/qoob-full/img/gallery-add.fea62b93.svg"
+  },
+  {
+    "revision": "f0e6d71bdf32b035ba705cdea504ef07",
+    "url": "/qoob-full/img/info-circle.f0e6d71b.svg"
+  },
+  {
+    "revision": "2e29d2ccdda5e9b05a4320f1118ebdca",
+    "url": "/qoob-full/img/information.2e29d2cc.svg"
+  },
+  {
+    "revision": "ec4a430a1fe4c9a8044af68869205bf5",
+    "url": "/qoob-full/img/instruction.ec4a430a.svg"
+  },
+  {
+    "revision": "a11d1774b09036afaca9b14152028cec",
+    "url": "/qoob-full/img/logout.a11d1774.svg"
+  },
+  {
+    "revision": "d34d7eb1e0e34e7dd8194aa06dbee12f",
+    "url": "/qoob-full/img/mainIndicators.d34d7eb1.jpg"
+  },
+  {
+    "revision": "f5f3fa8d4e1fec0e86cb97282806e075",
+    "url": "/qoob-full/img/menu.f5f3fa8d.svg"
+  },
+  {
+    "revision": "05a541142ffeb2660af9f3afbae1f860",
+    "url": "/qoob-full/img/mobileApp1.05a54114.png"
+  },
+  {
+    "revision": "f634b2feb97a831f1ab4d8bf88adf44f",
+    "url": "/qoob-full/img/qoob.store.f634b2fe.svg"
+  },
+  {
+    "revision": "456c9b5514220c8ecec34a9d8315b6b0",
+    "url": "/qoob-full/img/redo.456c9b55.svg"
+  },
+  {
+    "revision": "918d8056d934fa42340dda17b15b8fb3",
+    "url": "/qoob-full/img/search-normal.918d8056.svg"
+  },
+  {
+    "revision": "6636c59cb2168dbfb9426a9c4c73b779",
+    "url": "/qoob-full/img/services-segments.6636c59c.jpg"
+  },
+  {
+    "revision": "d31a986f8155d1486fcb12944ef5c4c8",
+    "url": "/qoob-full/img/setting.d31a986f.svg"
+  },
+  {
+    "revision": "f287f7c8062e670a27f565c944f12aa5",
+    "url": "/qoob-full/img/smile0.f287f7c8.png"
+  },
+  {
+    "revision": "3d4172ca982ee2136c016db838e4b600",
+    "url": "/qoob-full/img/smile1.3d4172ca.png"
+  },
+  {
+    "revision": "4afc59cd789b5133cc2819d44701468b",
+    "url": "/qoob-full/img/smile2.4afc59cd.png"
+  },
+  {
+    "revision": "605768270b7442281f4c3e1c46966461",
+    "url": "/qoob-full/img/smile3.60576827.png"
+  },
+  {
+    "revision": "31901f1da77384d7e30842275b7103d5",
+    "url": "/qoob-full/img/smile4.31901f1d.png"
+  },
+  {
+    "revision": "c16c8929165579df0691fc9632d927a5",
+    "url": "/qoob-full/img/smile5.c16c8929.png"
+  },
+  {
+    "revision": "2aca4c79cec6494658a0584c954d7ab3",
+    "url": "/qoob-full/img/smile6.2aca4c79.png"
+  },
+  {
+    "revision": "2772b9cff20d8b73e630b349e759f0f1",
+    "url": "/qoob-full/img/smile7.2772b9cf.png"
+  },
+  {
+    "revision": "dd6f268004c26b8cb6d20db5fb734403",
+    "url": "/qoob-full/img/smile8.dd6f2680.png"
+  },
+  {
+    "revision": "dd6f268004c26b8cb6d20db5fb734403",
+    "url": "/qoob-full/img/smile9.dd6f2680.png"
+  },
+  {
+    "revision": "79ce245e1e8bb6a25deae1da03576bf8",
+    "url": "/qoob-full/img/sms-tracking-bold.79ce245e.svg"
+  },
+  {
+    "revision": "5a937e7591aae22871969e9361ef4c18",
+    "url": "/qoob-full/img/sms-tracking_black.5a937e75.svg"
+  },
+  {
+    "revision": "42261501e3927129ac043d12515ded50",
+    "url": "/qoob-full/img/sms.42261501.svg"
+  },
+  {
+    "revision": "c594c06ae9d8da46bcc70c4067278c36",
+    "url": "/qoob-full/img/startPageImg.c594c06a.png"
+  },
+  {
+    "revision": "8ebbe850c2669be35d3d6f1263a2d91a",
+    "url": "/qoob-full/img/status-up.8ebbe850.svg"
+  },
+  {
+    "revision": "5a8bc1e4fe6bef5db23b33320286248c",
+    "url": "/qoob-full/img/tick-circle-y.5a8bc1e4.svg"
+  },
+  {
+    "revision": "dc141f97e37b4f5bb891caf175eded90",
+    "url": "/qoob-full/img/u-kassa.dc141f97.png"
+  },
+  {
+    "revision": "dbae69bb8a4502f8800bc946418e5058",
+    "url": "/qoob-full/img/user-edit.dbae69bb.svg"
+  },
+  {
+    "revision": "ddb797738137008d1031e1e6b9f0665a",
+    "url": "/qoob-full/img/var0.ddb79773.png"
+  },
+  {
+    "revision": "66c0f22d1742e56154e9d528e2456f9e",
+    "url": "/qoob-full/img/var1.66c0f22d.png"
+  },
+  {
+    "revision": "7039c76a2806513f355e814372eab51b",
+    "url": "/qoob-full/img/var10.7039c76a.png"
+  },
+  {
+    "revision": "ebe91d2c7de53c7e8c1c609432d315f1",
+    "url": "/qoob-full/img/var11.ebe91d2c.png"
+  },
+  {
+    "revision": "4eb28ce273c4dcf9924295dab79910c0",
+    "url": "/qoob-full/img/var2.4eb28ce2.png"
+  },
+  {
+    "revision": "18581a30ce12b9d95ae1d15a693c14ed",
+    "url": "/qoob-full/img/var3.18581a30.png"
+  },
+  {
+    "revision": "8da85281fe63fe0567c0dc4a72fe5a5f",
+    "url": "/qoob-full/img/var4.8da85281.png"
+  },
+  {
+    "revision": "a063a35ccc91bd961da8a8b254db07d4",
+    "url": "/qoob-full/img/var5.a063a35c.png"
+  },
+  {
+    "revision": "eaa64b1e50cb161b320a44b6cf66de26",
+    "url": "/qoob-full/img/var6.eaa64b1e.png"
+  },
+  {
+    "revision": "4cf6632083d6ff9c8dcf5c6038b9191d",
+    "url": "/qoob-full/img/var7.4cf66320.png"
+  },
+  {
+    "revision": "b171a37f873a72914a60ddf0ea3753f7",
+    "url": "/qoob-full/img/var8.b171a37f.png"
+  },
+  {
+    "revision": "ec9701bc5f75f48ff338671fe4fca7bd",
+    "url": "/qoob-full/img/var9.ec9701bc.png"
+  },
+  {
+    "revision": "c7c677565cccf1a607de9ae7e52edb22",
+    "url": "/qoob-full/img/whatsappLogo.c7c67756.png"
+  },
+  {
+    "revision": "c1b37589c05854e373e4f1f7d6ee7897",
+    "url": "/qoob-full/img/yclients-logo.c1b37589.svg"
+  },
+  {
+    "revision": "2ae0b5da6f74b5cfa7d0ff45f940504a",
+    "url": "/qoob-full/index.html"
+  },
+  {
+    "revision": "37fc888dbf6d274cf4a6",
+    "url": "/qoob-full/js/app.7e622103.js"
+  },
+  {
+    "revision": "3ee36787a3be214477c0",
+    "url": "/qoob-full/js/chunk-vendors.295dec5f.js"
+  },
+  {
+    "revision": "e7575ce8b70bc93733858044a5974e3a",
+    "url": "/qoob-full/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/qoob-full/robots.txt"
+  }
+]);
